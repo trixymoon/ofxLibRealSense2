fe32f7cd11211d74e229e4980777e55f30410b58
m9dfukc <andreas@invertednothing.com>
2019-02-08 11:48:10 +0100

    Added a pointcloud example

---
+++ example_pointCloud/src/ofApp.cpp
@@ -0,0 +1,60 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    ofSetVerticalSync(false);
+    _realsense2.setupDevice(0);
+    _realsense2.setupDepth(480, 270, 15);
+    _realsense2.enablePointcloud(true);
+    _realsense2.startPipeline(true);
+    
+    _gui.setup("appSettings.xml");
+    _gui.add(_realsense2.getGui());
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    _realsense2.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(120);
+    
+    ofSetColor(255);
+    if(_realsense2.depthEnabled()) {
+        _realsense2.getDepthTex()->draw(0, 0);
+    }
+    
+    
+    _cam.begin();
+    float s = 200;
+    ofScale(s,-s,-s);
+    ofDrawAxis(1);
+    
+    ofPushMatrix();
+    ofTranslate(0, 1, 0);
+    ofRotateZDeg(90);
+    ofSetColor(0, 200);
+    ofDrawGridPlane(1, 5, true);
+    ofPopMatrix();
+    
+    if (_realsense2.pointcloudEnabled()) {
+        _realsense2.getVboMesh()->drawVertices();
+    }
+    
+    _cam.end();
+    ofDrawBitmapString(ofToString(ofGetFrameRate()), 10, 10);
+    _gui.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+void ofApp::exit()
+{
+    _realsense2.exit();
+}
---
+++ example_pointCloud/src/ofApp.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxLibRealSense2.hpp"
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    void keyPressed(int key);
+    
+    ofxLibRealSense2 _realsense2;
+    ofxPanel         _gui;
+    ofEasyCam        _cam;
+};
